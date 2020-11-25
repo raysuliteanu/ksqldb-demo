d88f0d6980de9b283f500fb3a230a3e7f30180fb
Ray Suliteanu <raysuliteanu@gmail.com>
2020-11-25 12:03:51 -0800

    add webflux test

--- src/test/java/org/kidoni/ksqldbdemo/KsqldbDemoApplicationTests.java
+++ src/test/java/org/kidoni/ksqldbdemo/KsqldbDemoApplicationTests.java
@@ -2,15 +2,21 @@ package org.kidoni.ksqldbdemo;
 
 import java.io.File;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ActiveProfiles("test")
 @Testcontainers
+@AutoConfigureWebTestClient
 @SpringBootTest
 class KsqldbDemoApplicationTests {
     @Container
@@ -21,8 +27,24 @@ class KsqldbDemoApplicationTests {
                             Wait.forHealthcheck())
                     .withLocalCompose(true);
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void infoHandler() {
+        webTestClient.get()
+                     .uri("/info")
+                     .exchange()
+                     .expectStatus().isOk()
+                     .expectBody(String.class)
+                     .consumeWith(result -> {
+                         String responseBody = result.getResponseBody();
+                         assertThat(responseBody).contains("cluster_id", "ksql_service_id", "server_status", "version");
+                     });
+
+    }
 }
